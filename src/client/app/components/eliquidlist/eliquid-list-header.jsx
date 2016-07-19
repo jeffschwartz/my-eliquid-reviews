753813a04cd4e9ef854c952ba322d199aff7d35e
Jeff Schwartz <jefftschwartz@gmail.com>
2016-07-19 08:30:28 -0400

    Refactored component to a stateless function.

--- src/client/app/components/eliquidlist/eliquid-list-header.jsx
+++ src/client/app/components/eliquidlist/eliquid-list-header.jsx
@@ -1,58 +1,45 @@
-import React from "react";
-
-export default class EliquidListHeader extends React.Component {
-    constructor (props) {
-        super(props);
-        this.state = {
-            selectedOption: this.props.defaultOrder
-        };
-        this.handleAddButtonClick = this.handleAddButtonClick.bind(this);
-        this.handleOptionsChange = this.handleOptionsChange.bind(this);
-    }
-    handleOptionsChange (e) {
-        this.setState({selectedOption: e.target.value},
-            () => console.log("this.state.selectedOption changed. Now = ", this.state.selectedOption));
+const EliquidListHeader = (props) => {
+    let headerContainerStyle = {
+        minHeight: "2em",
+        lineHeight: "2em",
+        padding: "0 10px",
+        borderBottom: "solid 1px #000",
+        overflow: "hidden"
+    };
+    let headerStyle = {
+        display: "inline-block",
+        margin: "0 20px 0 0",
+        fontSize: "2em"
+    };
+    let buttonContainerStyle = {
+        display: "inline-block"
+    };
+    let addButtonStyle = {
+        marginLeft: "20px"
+    };
+    let handleOptionsChange = (e) => {
         // call owning component's handler
-        this.props.orderByChangedHandler(e.target.value);
-    }
-    handleAddButtonClick (e) {
+        props.orderByChangedHandler(e.target.value);
+    };
+    let handleAddButtonClick = (e) => {
         console.log("add button clicked");
-    }
-    render () {
-        let headerContainerStyle = {
-            minHeight: "2em",
-            lineHeight: "2em",
-            padding: "0 10px",
-            borderBottom: "solid 1px #000",
-            overflow: "hidden"
-        };
-        let headerStyle = {
-            display: "inline-block",
-            margin: "0 20px 0 0",
-            fontSize: "2em"
-        };
-        let buttonContainerStyle = {
-            display: "inline-block"
-        };
-        let addButtonStyle = {
-            marginLeft: "20px"
-        };
-        return (
-            <div style={headerContainerStyle}>
-                <h1 style={headerStyle}>Eliquids</h1>
-                <div style={buttonContainerStyle}>
-                    <span>order by: </span>
-                    <select onChange={this.handleOptionsChange} defaultValue={this.props.defaultOrder}>
-                        <option value="name,a">name - ascending</option>
-                        <option value="name,d">name - descending</option>
-                        <option value="category,a">category - ascending</option>
-                        <option value="category,d">category - descending</option>
-                        <option value="rating,a">rating - ascending</option>
-                        <option value="rating,d">rating - descending</option>
-                    </select>
-                    <button style={addButtonStyle} onClick={this.handleAddButtonClick}>Add Eliquid</button>
-                </div>
+    };
+    return (
+        <div style={headerContainerStyle}>
+            <h1 style={headerStyle}>Eliquids</h1>
+            <div style={buttonContainerStyle}>
+                <span>order by: </span>
+                <select onChange={handleOptionsChange} defaultValue={props.defaultOrder}>
+                    <option value="name,a">name - ascending</option>
+                    <option value="name,d">name - descending</option>
+                    <option value="category,a">category - ascending</option>
+                    <option value="category,d">category - descending</option>
+                    <option value="rating,a">rating - ascending</option>
+                    <option value="rating,d">rating - descending</option>
+                </select>
+                <button style={addButtonStyle} onClick={handleAddButtonClick}>Add Eliquid</button>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
+export default EliquidListHeader;
