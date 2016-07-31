a00797df38317365ca52ef3b667d69b783a04826
Jeff Schwartz <jefftschwartz@gmail.com>
2016-07-31 05:53:28 -0400

    Now calling the async action creator 'bootstrapEliquids' to populate the store.

--- src/client/app/components/eliquidlist/eliquid-list-container.jsx
+++ src/client/app/components/eliquidlist/eliquid-list-container.jsx
@@ -1,25 +1,20 @@
 import React from "react";
 import { connect } from "react-redux";
+import {bootstrapEliquids} from "../../redux/actions";
 import store from "../../redux/store";
-import {eLiquidsSuccess} from "../../redux/actions";
-import eliquidService from "../../services/eliquid-service";
 import EliquidList from "./eliquid-list";
 
 class EliquidListContainer extends React.Component {
     componentDidMount () {
         if (!this.props.eLiquids) {
-            console.log("calling eliquidService.get()");
-            eliquidService.get().then(result => {
-                console.log("EliquidListContainer promise resolved successfully");
-                store.dispatch(eLiquidsSuccess(result));
-            });
+            store.dispatch(bootstrapEliquids());
         }
     }
     render () {
         if (!this.props.eLiquids) {
             return false;
         }
-        console.log("EliquidListContainer yada render this.props", this.props);
+        // console.log("EliquidListContainer yada render this.props", this.props);
         return (
             <EliquidList eLiquids={this.props.eLiquids} defaultOrder="name,a" />
         );
