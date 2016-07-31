f0979a4eeb388882229b8afff755942c2f07e362
Jeff Schwartz <jefftschwartz@gmail.com>
2016-07-31 05:50:19 -0400

    Refactored actions.

--- src/client/app/redux/actions.js
+++ src/client/app/redux/actions.js
@@ -1,3 +1,39 @@
-export function eLiquidsSuccess (result) {
-    return {type: "ELIQUIDS_SUCCESS", result};
+import eliquidService from "../services/eliquid-service";
+
+export function eLiquidsIsFetching (isFetching) {
+    return {
+        type: "ELIQUIDS_ISFETCHING",
+        isFetching
+    };
 };
+
+export function eLiquidsSuccess (result) {
+    return {
+        type: "ELIQUIDS_SUCCESS",
+        result
+    };
+};
+
+export function eLiquidsFailure (error) {
+    return {
+        type: "ELIQUIDS_FAILURE",
+        error
+    };
+};
+
+// load eLiquids asynchronously and then
+// dispatch ELIQUIDS_SUCCESS or ELIQUIDS_ERROR
+export function bootstrapEliquids () {
+    return function (dispatch) {
+        dispatch(eLiquidsIsFetching(true));
+        eliquidService.get().then(
+            result => {
+                dispatch(eLiquidsSuccess(result));
+                dispatch(eLiquidsIsFetching(false));
+            },
+            error => {
+                dispatch(eLiquidsFailure(error));
+            }
+        );
+    };
+}
