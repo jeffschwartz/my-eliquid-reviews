358830c53957fb91822487d41fb9eb06e4a653d2
Jeff Schwartz <jefftschwartz@gmail.com>
2016-08-05 11:32:00 -0400

    Formatted file.

--- src/client/app/components/eliquidadd/eliquid-add.jsx
+++ src/client/app/components/eliquidadd/eliquid-add.jsx
@@ -3,7 +3,64 @@ import React from "react";
 export default class EliquidAdd extends React.Component {
     render () {
         return (
-            <h2>Add A New Eliquid</h2>
+            <form>
+                <div className="form-container">
+                    <h2>Add A New Eliquid</h2>
+                    <div className="form-item-container">
+                        <div className="label-container">
+                            <label htmlFor="name">Name: </label>
+                        </div>
+                        <div className="input-container">
+                            <input id="name" className="input-container__input" type="text" placeholder="enter name" autoFocus="true" />
+                        </div>
+                    </div>
+                    <div className="form-item-container">
+                        <div className="label-container">
+                            <label htmlFor="company">Compay: </label>
+                        </div>
+                        <div className="input-container">
+                            <input id="company" className="input-container__input" type="text" placeholder="enter company"/>
+                        </div>
+                    </div>
+                    <div className="form-item-container">
+                        <div className="label-container">
+                            <label htmlFor="category">Category: </label>
+                        </div>
+                        <div className="input-container">
+                            <input id="category" className="input-container__input" type="text" placeholder="enter category"/>
+                        </div>
+                    </div>
+                    <div className="form-item-container">
+                        <div className="label-container">
+                            <label htmlFor="rating">Rating: </label>
+                        </div>
+                        <div className="input-container">
+                            <input id="rating" className="input-container__input" type="text" placeholder="enter rating"/>
+                        </div>
+                    </div>
+                    <div className="form-item-container">
+                        <div className="label-container">
+                            <label htmlFor="vgpg">VG/PG: </label>
+                        </div>
+                        <div className="input-container">
+                            <input id="vgpg" className="input-container__input" type="text" placeholder="enter vg/pg"/>
+                        </div>
+                    </div>
+                    <div className="form-item-container">
+                        <div className="label-container">
+                            <label htmlFor="nic">Nic: </label>
+                        </div>
+                        <div className="input-container">
+                            <input id="nic" className="input-container__input" type="text" placeholder="enter nic"/>
+                        </div>
+                    </div>
+                    <div className="form-item-container">
+                        <div className="button-container">
+                            <button>Save</button> <button>Cancel</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
         );
     }
 }
