ee8713e1636a6ca0a030a4ffa537fb5442cecf1f
Jeff Schwartz <jefftschwartz@gmail.com>
2016-08-13 10:00:23 -0400

    Showing an alert box of passed values on form submit.

--- src/client/app/components/eliquidadd/eliquid-add-container.jsx
+++ src/client/app/components/eliquidadd/eliquid-add-container.jsx
@@ -2,7 +2,7 @@ import EliquidAddForm from "./eliquid-add-form";
 
 const EliquidAddContainer = () => {
     const handleSubmit = data => {
-        console.log("handleSubmit called, data =", data);
+        alert("handleSubmit called, data = " + JSON.stringify(data));
     };
     return (
         <EliquidAddForm onSubmit={handleSubmit}/>
