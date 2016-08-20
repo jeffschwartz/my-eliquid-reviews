1c4585067f6d02a2e36a745854b2d63fcc134629
Jeff Schwartz <jefftschwartz@gmail.com>
2016-08-20 09:40:12 -0400

    Using more concise es6 function features.

--- src/client/redux/actions.js
+++ src/client/redux/actions.js
@@ -1,39 +1,29 @@
 import eliquidService from "../services/eliquid-data-service";
 
-export const eLiquidsIsFetching = (isFetching) => {
-    return {
-        type: "ELIQUIDS_ISFETCHING",
-        isFetching
-    };
-};
+export const eLiquidsIsFetching = isFetching => ({
+    type: "ELIQUIDS_ISFETCHING",
+    isFetching
+});
 
-export const eLiquidsSuccess = (result) => {
-    return {
-        type: "ELIQUIDS_SUCCESS",
-        result
-    };
-};
+export const eLiquidsSuccess = result => ({
+    type: "ELIQUIDS_SUCCESS",
+    result
+});
 
-export const eLiquidsFailure = (error) => {
-    return {
-        type: "ELIQUIDS_FAILURE",
-        error
-    };
-};
+export const eLiquidsFailure = error => ({
+    type: "ELIQUIDS_FAILURE",
+    error
+});
 
-export const eLiquidsAddedNew = (eLiquid) => {
-    return {
-        type: "ELIQUIDS_ADDED_NEW",
-        eLiquid
-    };
-};
+export const eLiquidsAddedNew = eLiquid => ({
+    type: "ELIQUIDS_ADDED_NEW",
+    eLiquid
+});
 
-export const eLiquidListSortOrderSelected = (orderBy) => {
-    return {
-        type: "ELIQUID_LIST_SORT_ORDER_SELECTED",
-        orderBy
-    };
-};
+export const eLiquidListSortOrderSelected = orderBy => ({
+    type: "ELIQUID_LIST_SORT_ORDER_SELECTED",
+    orderBy
+});
 
 // loads eLiquids asynchronously & then dispatches either
 // ELIQUIDS_SUCCESS or ELIQUIDS_ERROR based on success or
