1b3a444cc46fb55ccee30607b9cae712c9f09dc2
Jeff Schwartz <jefftschwartz@gmail.com>
2016-09-02 18:39:38 -0400

    Updated redux-form from v6.0.0-rc.4 to v6.0.2.

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,10 +28,12 @@ module.exports = {
         "semi": ["error", "always"],
         "quotes": ["error", "double"],
         "indent": ["error", 4],
+        "no-unused-vars": 0,
         "react/jsx-uses-vars": "error",
         "eol-last": 0,
         "no-class-assign": 0,
         "react/prop-types": 0,
-        "react/react-in-jsx-scope": 0
+        "react/react-in-jsx-scope": 0,
+        "react/no-danger": 0
     }
 };
