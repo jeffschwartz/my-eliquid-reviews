cf371ca6319e6fa2136b6ffbdd6ceb4de667352b
Jeff Schwartz <jefftschwartz@gmail.com>
2016-09-03 13:37:18 -0400

    Added footer with centered copyright.

--- src/client/components/app.jsx
+++ src/client/components/app.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import AppNavbar from "../components/navbar/appnavbar";
 import NavLink from "../components/navlink/navlink";
 import Container from "./container/container";
+import Footer from "../components/footer/footer";
 
 const App = (props) => (
     <div>
@@ -10,6 +11,7 @@ const App = (props) => (
             <NavLink to="/">Home</NavLink>
         </Container>
         {props.children}
+        <Footer />
     </div>
 );
 
